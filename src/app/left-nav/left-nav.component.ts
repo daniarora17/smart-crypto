9ed84c93ce4b72a59d7b867fb93e25ef6deaa15d
Danish Arora <danisharora@Danishs-MacBook-Air.local>
2018-01-12 00:46:13 +0530

    left nav updated

--- src/app/left-nav/left-nav.component.ts
+++ src/app/left-nav/left-nav.component.ts
@@ -6,6 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./left-nav.component.less']
 })
 export class LeftNavComponent implements OnInit {
+  coinList: Array<any> = [
+    {
+      'key': 'btc',
+      'name': 'Bit Coin'
+    },
+    {
+      'key': 'rip',
+      'name': 'Ripple'
+    }
+  ];
 
   constructor() { }
 
